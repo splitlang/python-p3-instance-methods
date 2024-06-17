a3e351cdee0196a948b851a3f9f22dd736ceaaef
splitlang <seanmark.student@moringaschool>
2024-06-17 23:26:57 +0300

    completed

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
-
 class Dog:
-    # Class body goes here
+    def __init__(self, name='Dog'):
+        self.name = name
 
-    #Instance method definition
-    pass
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        if self.name == 'Dog':
+         print("The dog is sitting.")
+        else:
+           print(f"The dog {self.name} is sitting.")
